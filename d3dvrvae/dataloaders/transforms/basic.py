f574fc48e14119bdc52fca15eb738b8c0b8b962a
nnaakkaaii <ultraganbayu10@gmail.com>
2024-06-04 19:36:45 +0900

    wip in fb autoencoder

--- d3dvrvae/dataloaders/transforms/basic.py
+++ d3dvrvae/dataloaders/transforms/basic.py
@@ -14,11 +14,12 @@ class BasicTransformOption(TransformOption):
 
 
 def create_basic_transform(opt: BasicTransformOption) -> Transform:
-    ts = [transforms.ToTensor()]
-    if opt.normalize:
-        ts.append(transforms.Normalize((0.5,), (0.5,)))
+    ts = []
     if opt.pad_size:
         ts.append(transforms.Pad(opt.pad_size, fill=0))
+    ts.append([transforms.ToTensor()])
+    if opt.normalize:
+        ts.append(transforms.Normalize((0.5,), (0.5,)))
     if opt.crop_size:
         ts.append(transforms.RandomCrop(opt.crop_size))
     return transforms.Compose(ts)
