dd1e6e9343906ad6d9a4251c95d7da0eb766a9d9
nnaakkaaii <ultraganbayu10@gmail.com>
2024-06-04 19:07:27 +0900

    update transform

--- d3dvrvae/dataloaders/transforms/basic.py
+++ d3dvrvae/dataloaders/transforms/basic.py
@@ -9,10 +9,16 @@ from .typing import Transform
 @dataclass
 class BasicTransformOption(TransformOption):
     normalize: bool = True
+    pad_size: int | None = None
+    crop_size: int | None = None
 
 
 def create_basic_transform(opt: BasicTransformOption) -> Transform:
     ts = [transforms.ToTensor()]
     if opt.normalize:
         ts.append(transforms.Normalize((0.5,), (0.5,)))
+    if opt.pad_size:
+        ts.append(transforms.Pad(opt.pad_size))
+    if opt.crop_size:
+        ts.append(transforms.RandomCrop(opt.crop_size))
     return transforms.Compose(ts)
