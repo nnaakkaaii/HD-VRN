bd85d0b8b5f13411742895766152ad6db780d216
nnaakkaaii <ultraganbayu10@gmail.com>
2024-06-15 18:29:38 +0900

    update save image function

--- hrdae/models/functions.py
+++ hrdae/models/functions.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def save_reconstructed_images(
-    original: np.ndarray, reconstructed: np.ndarray, epoch: int, save_dir: Path
-):
-    save_dir.mkdir(parents=True, exist_ok=True)
+def _save_images(
+    original: np.ndarray,
+    reconstructed: np.ndarray,
+    save_path: Path,
+) -> None:
+    # (b, 1, h, w)
+    save_path.mkdir(parents=True, exist_ok=True)
     plt.figure(figsize=(20, 4))
 
     # Original Images
@@ -25,5 +28,47 @@ def save_reconstructed_images(
         plt.axis("off")
 
     # 保存するファイル名を設定
-    plt.savefig(save_dir / f"original_epoch_{epoch}.png")
+    plt.savefig(save_path)
     plt.close()
+
+
+def save_reconstructed_videos(
+    original: np.ndarray,
+    reconstructed: np.ndarray,
+    epoch: int,
+    save_dir: Path
+):
+    if len(original.shape) == 4:
+        _save_images(
+            original,
+            reconstructed,
+            save_dir / f"epoch_{epoch}.png",
+        )
+    elif len(original.shape) == 5:
+        b, _, _, _, _ = original.shape
+        for bi in range(0, b, 5):
+            _save_images(
+                original[b],  # (t, 1, h, w)
+                reconstructed[b],  # (t, 1, h, w)
+                save_dir / f"epoch_{epoch}_batch_{bi}.png",
+            )
+    elif len(original.shape) == 6:
+        b, _, _, d, h, w = original.shape
+        for bi in range(0, b, 5):
+            _save_images(
+                original[b, :, :, :, w // 2],  # (t, 1, d, h)
+                reconstructed[b, :, :, :, w // 2],  # (t, 1, d, h)
+                save_dir / f"epoch_{epoch}_batch_{bi}_axis_x.png",
+                )
+            _save_images(
+                original[b, :, :, h // 2],  # (t, 1, d, w)
+                reconstructed[b, :, :, h // 2],  # (t, 1, d, w)
+                save_dir / f"epoch_{epoch}_batch_{bi}_axis_y.png",
+                )
+            _save_images(
+                original[b, :, d // 2],  # (t, 1, h, w)
+                reconstructed[b, :, d // 2],  # (t, 1, h, w)
+                save_dir / f"epoch_{epoch}_batch_{bi}_axis_z.png",
+                )
+    else:
+        raise ValueError("Invalid shape of original and reconstructed videos")
