6553a711b9c76913c870ec18502deaa24cfa0854
nnaakkaaii <ultraganbayu10@gmail.com>
2024-07-14 14:29:22 +0900

    impl test for triplet loss

---
+++ test/models/losses/test_triplet.py
@@ -0,0 +1,23 @@
+from torch import randn
+
+from hrdae.models.losses import create_loss, TripletLossOption
+
+
+def test_triplet_loss():
+    triplet_loss = create_loss(TripletLossOption())
+    b, n, c, w, h = 8, 10, 8, 4, 4
+
+    input = randn(b, n, w, h)
+    target = randn(b, n, w, h)
+    latent = randn(b, 1, c, w, h)
+    positive = randn(b, n, c, w, h)
+    negative = randn(b, n, c, w, h)
+
+    loss = triplet_loss(
+        input,
+        target,
+        latent=[latent],
+        positive=[positive],
+        negative=[negative],
+    )
+    assert loss.size() == ()
